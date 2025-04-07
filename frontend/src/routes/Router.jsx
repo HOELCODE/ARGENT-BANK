11aeb98283be0fe55d9fa735c4a66fb23c585c85
HOELCODE <hcostantini1@gmail.com>
2025-04-07 16:48:48 +0200

    différentes pages misent en place, html

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -2,11 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages
 import Home from "../pages/Home";
+import SignIn from "../pages/Sign-in";
+import User from "../pages/User";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/profile" element={<User />} />
         </Routes>
     );
 }
