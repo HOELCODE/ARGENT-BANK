0e56369f1542fda01288e569aa0d239336585a1e
HOELCODE <hcostantini1@gmail.com>
2025-04-07 16:13:52 +0200

    Configuration react + Page d'acceuil

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './layout/Navbar.jsx';
+import AppRouter from './routes/Router.jsx';
+
+import './css/main.css'
+
+const App = () => {
+
+  return (
+    <>
+    <Navbar />
+    <AppRouter />
+    </>
+  )
+}
+
+export default App
