b4058b1653e6562fa9e7562143bee6f346180b4c
HOELCODE <hcostantini1@gmail.com>
2025-04-08 15:14:30 +0200

    Ajout de Provider dans main et gestion de login logout dans Navbar

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
 import logo from "../images/argentBankLogo.png";
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../redux/authSlice.js';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+  
+    const token = useSelector((state) => state.auth.token)
+  
+    const handleLogout = () => {
+      dispatch(logout())
+      navigate('/') // Redirection vers la page d'accueil après déconnexion
+    }
     
     return (
         <nav className="main-nav">
@@ -14,10 +27,17 @@ const Navbar = () => {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link className="main-nav-item" to="/sign-in">
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
-                </Link>
+                {token ? (
+                    <Link className="main-nav-item" to="/" onClick={handleLogout}>
+                        <i className="fa fa-sign-out"></i>
+                        Sign Out
+                    </Link>
+                ) : (
+                    <Link className="main-nav-item" to="/sign-up">
+                        <i className="fa fa-user-plus"></i>
+                        Sign In
+                    </Link>
+                )}
             </div>
         </nav>
     )
