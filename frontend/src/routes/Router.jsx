5c985f4e67272d63793c976cfe11e5898787beaf
HOELCODE <hcostantini1@gmail.com>
2025-04-08 16:13:04 +0200

    Création de PrivateRoute pour gérer login et modification de Router.jsx pour ajouter PrivateRoute

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -4,15 +4,23 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/Sign-in";
 import User from "../pages/User";
+import PrivateRoute from "./PrivateRoute";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/profile" element={<User />} />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <User />
+                </PrivateRoute>
+              }
+            />
         </Routes>
-    );
+    )
 }
 
 export default AppRouter;
