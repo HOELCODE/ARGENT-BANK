79c3faa8cc932c816d552a02ed1b9d9fb095c8c4
HOELCODE <hcostantini1@gmail.com>
2025-04-18 15:52:04 +0200

    projet ok

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,21 +1,34 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/argentBankLogo.png";
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../redux/authSlice.js';
+import { fetchUserProfile } from "../redux/authSlice.js";
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
-  
-    const token = useSelector((state) => state.auth.token)
-  
+
+    const {token, user, loading} = useSelector((state) => state.auth)
+
     const handleLogout = () => {
-      dispatch(logout())
-      navigate('/') // Redirection vers la page d'accueil après déconnexion
+        dispatch(logout())
+        navigate('/') // Redirection vers la page d'accueil après déconnexion
     }
-    
+
+    // Gérer l'affichage deu prenom dans la nav quand le user est connecté 
+    useEffect(() => {
+        if (token) {
+            dispatch(fetchUserProfile(token));
+        }
+    }, [token, dispatch]);
+
+    if (loading || !user) {
+        return <p>Chargement...</p>
+    }
+      
     return (
         <nav className="main-nav">
             <Link className="main-nav-logo" to="/">
@@ -28,10 +41,14 @@ const Navbar = () => {
             </Link>
             <div>
                 {token ? (
-                    <Link className="main-nav-item" to="/" onClick={handleLogout}>
-                        <i className="fa fa-sign-out"></i>
-                        Sign Out
-                    </Link>
+                    <div className="nav-logged">
+                        <i className="fa-solid fa-circle-user"></i>
+                        <span>{user.firstName}</span>
+                        <Link className="main-nav-item" to="/" onClick={handleLogout}>
+                            <i className="fa fa-sign-out"></i>
+                            Sign Out
+                        </Link>
+                    </div>
                 ) : (
                     <Link className="main-nav-item" to="/sign-in">
                         <i className="fa fa-user-plus"></i>
